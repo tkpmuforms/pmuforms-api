944c4e1a53401bbe01cb5ae0e6728c74a0bab7eb
Akinola <60165552+Akinolaaa@users.noreply.github.com>
2024-11-19 00:20:05 +0100

    update: customer and user schema updates

--- src/database/schema/customer.schema.ts
+++ src/database/schema/customer.schema.ts
@@ -4,18 +4,41 @@ import { HydratedDocument } from 'mongoose';
 export type CustomerDocument = HydratedDocument<Customer>;
 
 @Schema()
+export class Service {
+  @Prop()
+  id: number;
+
+  @Prop()
+  service: string;
+}
+
+export const ServiceSchema = SchemaFactory.createForClass(Service);
+
+@Schema({ timestamps: true })
 export class Customer {
+  @Prop()
+  appStorePurchaseActive: boolean;
+
   @Prop({ required: true })
+  businessName: string;
+
+  @Prop()
+  clients: string[];
+
+  @Prop()
   email: string;
 
   @Prop()
-  password: string;
+  isActive: boolean;
+
+  @Prop({ type: Date, default: null })
+  lastLoggedIn: Date;
+
+  @Prop({ type: [ServiceSchema] })
+  services: Service[];
 
   @Prop()
-  firstName: string;
-
-  @Prop()
-  lastName: number;
+  userId: string;
 }
 
 export const CustomerSchema = SchemaFactory.createForClass(Customer);
