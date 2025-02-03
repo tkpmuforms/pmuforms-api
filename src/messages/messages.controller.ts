5dc5621e98f02387999c4b53e0d77a5bf78b5555
Akinola <60165552+Akinolaaa@users.noreply.github.com>
2025-02-03 13:31:34 +0100

    update: messages endpoint now public

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,10 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateNewMessageDto } from './dto';
 import { MessagesService } from './messages.service';
+import { Public } from 'src/auth/decorator';
 
 @Controller('api/messages')
 export class MessagesController {
   constructor(private readonly messageService: MessagesService) {}
+
+  @Public()
   @Post('/')
   async createNewMessage(@Body() dto: CreateNewMessageDto) {
     const message = await this.messageService.createNewMessage(dto);
