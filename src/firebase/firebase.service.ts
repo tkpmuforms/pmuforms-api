bbc8d264b4e4c0324bbbcc5ee8a0ca334761d0b4
taolak <taolak4orcl@gmail.com>
2025-04-07 18:33:48 -0500

    wrap firebase auth

--- src/firebase/firebase.service.ts
+++ src/firebase/firebase.service.ts
@@ -2,26 +2,26 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import firebaseAdmin from 'firebase-admin';
 import { getMessaging } from 'firebase-admin/messaging';
 import { getAuth } from 'firebase-admin/auth';
-import path from 'node:path';
 import { AppConfigService } from 'src/config/config.service';
 
 @Injectable()
 export class FirebaseService {
   constructor(private config: AppConfigService) {
-    // initialize firebase admin
-    const firebaseSirverAccoutCred = this.config.get('FIREBASE_SERVICE_ACCOUNT_JSON');
-
-    try {
-      const serviceAccount = JSON.parse(firebaseSirverAccoutCred);
-      firebaseAdmin.initializeApp({
-        credential: firebaseAdmin.credential.cert(
-          serviceAccount,
-        ),
-      });
-    } catch (error: any) {
-      console.info(`Failed to initialize firebase admin.
-        Suggestion: Create a file called '/secrets/firebase-service-account-credentials.json' in the project base directory with the appropriate firebase service account secrets from the firebase console. \n`);
-      console.error({ error });
+    if (!firebaseAdmin.apps.length) {
+      // initialize firebase admin
+      const firebaseSirverAccoutCred = this.config.get('FIREBASE_SERVICE_ACCOUNT_JSON');
+      try {
+        const serviceAccount = JSON.parse(firebaseSirverAccoutCred);
+        firebaseAdmin.initializeApp({
+          credential: firebaseAdmin.credential.cert(
+            serviceAccount,
+          ),
+        }, "pmu-api-001");
+      } catch (error: any) {
+        console.info(`Failed to initialize firebase admin.
+          Suggestion: Create a file called '/secrets/firebase-service-account-credentials.json' in the project base directory with the appropriate firebase service account secrets from the firebase console. \n`);
+        console.error({ error });
+      }
     }
   }
 
