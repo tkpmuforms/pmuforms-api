967d3a1117fbc23c6b62f3770c6d992f7ea910cd
taolak <taolak4orcl@gmail.com>
2025-06-29 18:48:34 -0500

    add filled forms validation

--- src/filled-forms/filled-forms.service.ts
+++ src/filled-forms/filled-forms.service.ts
@@ -33,13 +33,11 @@ export class FilledFormsService {
     userId: string,
     appointment: AppointmentDocument,
   ) {
-    console.log(JSON.stringify(appointment));
-    
-    // if (appointment.artistId !== userId && appointment.customerId !== userId) {
-    //   throw new ForbiddenException(
-    //     `You are not allowed to perform this action`,
-    //   );
-    // }
+    if (appointment.artistId !== userId && appointment.customerId !== userId) {
+      throw new ForbiddenException(
+        `You are not allowed to perform this action`,
+      );
+    }
   }
 
   async submitForm(
